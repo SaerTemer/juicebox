b7471c4493d35a5245831dbc4970ba8a2992ad0b
SaerTemer <terynmerillat@gmail.com>
2021-10-17 21:34:09 -0400

    I'm annoyed

--- db/seed.js
+++ db/seed.js
@@ -132,30 +132,6 @@ async function createInitialPosts() {
   }
 }
 
-async function createInitialTags() {
-  try {
-    console.log("Starting to create tags...");
-
-    const [happy, sad, inspo, catman] = await createTags([
-      '#happy', 
-      '#worst-day-ever', 
-      '#youcandoanything',
-      '#catmandoeverything'
-    ]);
-
-    const [postOne, postTwo, postThree] = await getAllPosts();
-
-    await addTagsToPost(postOne.id, [happy, inspo]);
-    await addTagsToPost(postTwo.id, [sad, inspo]);
-    await addTagsToPost(postThree.id, [happy, catman, inspo]);
-
-    console.log("Finished creating tags!");
-  } catch (error) {
-    console.log("Error creating tags!");
-    throw error;
-  }
-}
-
 async function rebuildDB() {
   try {
     client.connect();
@@ -164,7 +140,6 @@ async function rebuildDB() {
     await createTables();
     await createInitialUsers();
     await createInitialPosts();
-    await createInitialTags(); // new
   } catch (error) {
     console.log("Error during rebuildDB")
     throw error;
